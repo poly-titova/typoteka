41252b0894acf432ec0e72642e96562e57ca9e55
Poly Titova <poly_titova@mail.ru>
2020-07-14 10:25:58 +0300

    Создали статус-коды в constants и подключили

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,6 +1,9 @@
 'use strict';
-￼
-￼// подключим дополнительные пакеты
-￼const chalk = require(`chalk`);
-￼const http = require(`http`);
-￼const fs = require(`fs`).promises;
+
+// подключим дополнительные пакеты
+const chalk = require(`chalk`);
+const http = require(`http`);
+const fs = require(`fs`).promises;
+
+// подключим статус-коды
+const { HttpCode } = require(`../constants`);
