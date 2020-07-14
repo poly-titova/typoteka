e02204949287b748f2bc11658b828d66dd701c2b
Poly Titova <poly_titova@mail.ru>
2020-07-14 10:47:40 +0300

    Получили введённое пользователем значение и сделали запуск http-сервера

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -11,5 +11,17 @@ const { HttpCode } = require(`../constants`);
 module.exports = {
   name: `--server`,
   run(args) {
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    http
+      .createServer(onClientConnect)
+      .listen(port)
+      .on(`listening`, (err) => {
+        if (err) {
+          return console.error(chalk.red(`Ошибка при создании сервера`, err));
+        }
+        return console.log(chalk.green(`Ожидаю соединений по адресу http://localhost:${port}`));
+      });
   }
 }
