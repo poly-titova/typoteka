6cce3986aa96e9b8a08c159db06393d8c65a0460
Poly Titova <poly_titova@mail.ru>
2020-07-14 10:51:59 +0300

    Сделали функцию-отправки ответа клиентам

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,6 +8,24 @@ const fs = require(`fs`).promises;
 // подключим статус-коды
 const { HttpCode } = require(`../constants`);
 
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
 const onClientConnect = async (req, res) => {
   const notFoundMessageText = `Not found`;
 
