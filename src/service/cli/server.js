76553ee91e2127459f0d029c4d80eaf04da8fdf7
Poly Titova <poly_titova@mail.ru>
2020-07-14 10:49:32 +0300

    Сделали обработку запросов клиентов

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,6 +8,27 @@ const fs = require(`fs`).promises;
 // подключим статус-коды
 const { HttpCode } = require(`../constants`);
 
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
 module.exports = {
   name: `--server`,
   run(args) {
