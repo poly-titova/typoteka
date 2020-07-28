6ac1af071b275df9cd99b3fe4a4eeb4c4c66cd2b
Poly Titova <poly_titova@mail.ru>
2020-07-28 10:47:02 +0300

    Зарегистрировали маршруты в главном роуте

---
+++ src/express/routes/main-routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {Router} = require(`express`);
+const mainRoutes = new Router();
+
+mainRoutes.get(`/`, (req, res) => res.send(`/`));
+mainRoutes.get(`/register`, (req, res) => res.send(`/register`));
+mainRoutes.get(`/login`, (req, res) => res.send(`/login`));
+mainRoutes.get(`/search`, (req, res) => res.send(`/search`));
+mainRoutes.get(`/categories`, (req, res) => res.send(`/categories`));
+
+module.exports = mainRoutes; 
