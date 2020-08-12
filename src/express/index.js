44a8fd78e2318b67ddb3b3fd152d39a0cfb46354
Poly Titova <poly_titova@mail.ru>
2020-08-12 13:17:25 +0300

    Зафиксировали название директории, создали эту директорию и сконфигурировали Express

--- src/express/index.js
+++ src/express/index.js
@@ -12,6 +12,12 @@ const app = express();
 
 const path = require(`path`);
 
+const PUBLIC_DIR = `public`;
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
