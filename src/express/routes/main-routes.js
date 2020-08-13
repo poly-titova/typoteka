3600a37df004925bf9ea16e3a6957348670166d4
Poly Titova <poly_titova@mail.ru>
2020-08-13 16:39:38 +0300

    Добавили вход и регистрацию

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,8 +4,8 @@ const {Router} = require(`express`);
 const mainRoutes = new Router();
 
 mainRoutes.get(`/`, (req, res) => res.render(`main`));
-mainRoutes.get(`/register`, (req, res) => res.send(`/register`));
-mainRoutes.get(`/login`, (req, res) => res.send(`/login`));
+mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.send(`/search`));
 mainRoutes.get(`/categories`, (req, res) => res.send(`/categories`));
 
