4e98dc40a48e7084bd59b4954200192b2389e8bf
Poly Titova <poly_titova@mail.ru>
2020-08-13 15:31:26 +0300

    Добавили главную страничку

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const mainRoutes = new Router();
 
-mainRoutes.get(`/`, (req, res) => res.send(`/`));
+mainRoutes.get(`/`, (req, res) => res.render(`main`));
 mainRoutes.get(`/register`, (req, res) => res.send(`/register`));
 mainRoutes.get(`/login`, (req, res) => res.send(`/login`));
 mainRoutes.get(`/search`, (req, res) => res.send(`/search`));
