c58eb1f568aa943b536fe03f10168a7cc2f7ae58
Poly Titova <poly_titova@mail.ru>
2020-08-15 12:44:45 +0300

    Добавили страницу мои публикации

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const {Router} = require(`express`);
+const { Router } = require(`express`);
 const myRoutes = new Router();
 
-myRoutes.get(`/`, (req, res) => res.send(`/my`));
+myRoutes.get(`/`, (req, res) => res.render(`my`));
 myRoutes.get(`/comments`, (req, res) => res.send(`/my/comments`));
 
-module.exports = myRoutes; 
+module.exports = myRoutes;
