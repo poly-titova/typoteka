9e1d5e5fb674a13e366fab86024cfbef11a91fe3
Poly Titova <poly_titova@mail.ru>
2020-08-15 16:38:29 +0300

    Добавили страницу создания новой публикации

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,7 +4,7 @@ const { Router } = require(`express`);
 const articlesRoutes = new Router();
 
 articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
-articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
+articlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
