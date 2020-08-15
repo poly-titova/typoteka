99233c1a6a583a571b18707195674288afc64dcb
Poly Titova <poly_titova@mail.ru>
2020-08-15 16:06:07 +0300

    Добавили страницу публикации в определённой категории и исправили ссылки

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,9 +3,9 @@
 const { Router } = require(`express`);
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/category/:id`, (req, res) => res.render(`post`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRoutes;
