27d44c569a24c558b298c2d0f354b72def9d347f
Poly Titova <poly_titova@mail.ru>
2020-08-15 15:45:05 +0300

    Добавили страницу со списком всех категорий

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const {Router} = require(`express`);
+const { Router } = require(`express`);
 const mainRoutes = new Router();
 
 mainRoutes.get(`/`, (req, res) => res.render(`main`));
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search`));
-mainRoutes.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRoutes.get(`/categories`, (req, res) => res.render(`all-categories`));
 
-module.exports = mainRoutes; 
+module.exports = mainRoutes;
