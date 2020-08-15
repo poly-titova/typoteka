1bc07dcbd6d314721eb5b24eedd8053cabfac147
Poly Titova <poly_titova@mail.ru>
2020-08-15 16:47:51 +0300

    Добавили исключение для директории public

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -5,7 +5,6 @@ const articlesRoutes = new Router();
 
 articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
-articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRoutes;
