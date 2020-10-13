ee9cffb70a4925327cab2478b1b1657fd5227cfd
Poly Titova <poly_titova@mail.ru>
2020-10-13 21:00:10 +0300

    Заменили http сервер на express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const chalk = require(`chalk`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 
 const { HttpCode } = require(`../../constants`);
@@ -8,10 +9,29 @@ const { HttpCode } = require(`../../constants`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
+const app = express();
+app.use(express.json());
+
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+  }
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`))
+
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    app.listen(port, () => console.log(chalk.green(`Ожидаю соединений на ${port}`)));
   }
 }
