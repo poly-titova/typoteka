382bb36dc2eb4362a9947d5337fdbcb620e1be1f
Poly Titova <poly_titova@mail.ru>
2020-11-03 18:22:03 +0300

    Изменили структуру файла server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,25 +2,17 @@
 
 const chalk = require(`chalk`);
 const express = require(`express`);
-const fs = require(`fs`).promises;
+const routes = require(`../api`);
 
-const { HttpCode } = require(`../../constants`);
+const { HttpCode, API_PREFIX } = require(`../../constants`);
+const getMockData = require(`../lib/get-mock-data`);
 
 const DEFAULT_PORT = 3000;
-const FILENAME = `mocks.json`;
 
 const app = express();
-app.use(express.json());
 
-app.get(`/posts`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    res.send([]);
-  }
-});
+app.use(express.json());
+app.use(API_PREFIX, routes);
 
 app.use((req, res) => res
   .status(HttpCode.NOT_FOUND)
@@ -28,10 +20,23 @@ app.use((req, res) => res
 
 module.exports = {
   name: `--server`,
-  run(args) {
+  async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    app.listen(port, () => console.log(chalk.green(`Ожидаю соединений на ${port}`)));
+    try {
+      await getMockData();
+
+      app.listen(port, (err) => {
+        if (err) {
+          return console.error(`Ошибка при создании сервера`, err);
+        }
+
+        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+      });
+    } catch (err) {
+      console.error(`Произошла ошибка: ${err.message}`);
+      process.exit(1);
+    }
   }
 }
