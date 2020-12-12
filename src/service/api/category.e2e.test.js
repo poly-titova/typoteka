c9d0a7dbe9f1ba5d9b1b5c9f1d8ad939f6281cb0
Poly Titova <poly_titova@mail.ru>
2020-12-12 11:54:55 +0300

    Протестировали категории

--- src/service/api/category.e2e.test.js
+++ src/service/api/category.e2e.test.js
@@ -123,3 +123,23 @@ const mockData = [
 const app = express();
 app.use(express.json());
 category(app, new DataService(mockData));
+
+describe(`API returns category list`, () => {
+
+  let response;
+
+  beforeAll(async () => {
+    response = await request(app)
+      .get(`/category`);
+  });
+
+  test(`Status code 200`, () => expect(response.statusCode).toBe(HttpCode.OK));
+  test(`Returns list of 4 categories`, () => expect(response.body.length).toBe(4));
+
+  test(`Category names are "Рисование", "Еда", "Без рамки", "Программирование"`,
+    () => expect(response.body).toEqual(
+      expect.arrayContaining([`Рисование`, `Еда`, `Без рамки`, `Программирование`])
+    )
+  );
+
+});
--- src/service/api/category.js
+++ src/service/api/category.js
@@ -6,7 +6,7 @@ const { HttpCode } = require(`../../constants`);
 const route = new Router();
 
 module.exports = (app, service) => {
-  app.use(`/categories`, route);
+  app.use(`/category`, route);
 
   route.get(`/`, (req, res) => {
     const categories = service.findAll();
