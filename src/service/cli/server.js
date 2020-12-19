1f84d361e178aa50c4dc51acc8e2c173cc3d1c88
Poly Titova <poly_titova@mail.ru>
2020-12-19 11:35:39 +0300

    Написали middleware, чтобы выводить больше информации об API

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -12,12 +12,26 @@ const DEFAULT_PORT = 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(express.json());
 app.use(API_PREFIX, routes);
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`))
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occured on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
