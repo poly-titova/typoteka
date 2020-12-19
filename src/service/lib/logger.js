7a09690646ff63d229844d2185cc533accc13d83
Poly Titova <poly_titova@mail.ru>
2020-12-19 11:40:24 +0300

    Добавили переменные окружения

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,12 +1,17 @@
 "use strict";
 
 const pino = require(`pino`);
+const { Env } = require(`../../constants`);
+
+const LOG_FILE = `./logs/api.log`
+const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
+const defaultLogLevel = isDevMode ? `info` : `error`;
 
 const logger = pino({
   name: `base-logger`,
-  level: 'info',
-  prettyPrint: true
-});
+  level: process.env.LOG_LEVEL || defaultLogLevel,
+  prettyPrint: isDevMode
+}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
 
 module.exports = {
   logger,
