48743ed096b7a396393aa083293169f590ea2f02
Poly Titova <poly_titova@mail.ru>
2021-01-15 16:54:37 +0300

    Исправили ошибки

--- src/service/api/articles.js
+++ src/service/api/articles.js
@@ -6,9 +6,9 @@ const articleValidator = require(`../middlewares/article-validator`);
 const articleExist = require(`../middlewares/article-exists`);
 const commentValidator = require(`../middlewares/comment-validator`);
 
-const route = new Router();
-
 module.exports = (app, articleService, commentService) => {
+  const route = new Router();
+  
   app.use(`/articles`, route);
 
   // возвращает список публикаций
