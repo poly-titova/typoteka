67c0e27a7237eedb9c3bb36b645d80b2f66fad28
Poly Titova <poly_titova@mail.ru>
2021-02-21 13:43:04 +0300

    Сделали показ списка всех статей

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,8 +2,12 @@
 
 const { Router } = require(`express`);
 const mainRoutes = new Router();
+const api = require(`../api`).getAPI();
 
-mainRoutes.get(`/`, (req, res) => res.render(`main`));
+mainRoutes.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`main`, { articles });
+}); 
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search`));
