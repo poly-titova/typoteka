d2627469ee62454852cc2e87b12549cc88fd9a39
Poly Titova <poly_titova@mail.ru>
2021-02-21 14:23:23 +0300

    Сделали вывод собственных статей

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,8 +2,12 @@
 
 const { Router } = require(`express`);
 const myRoutes = new Router();
+const api = require(`../api`).getAPI();
 
-myRoutes.get(`/`, (req, res) => res.render(`my`));
+myRoutes.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`my`, { articles });
+}); 
 myRoutes.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRoutes;
