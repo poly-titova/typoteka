0e8e08ffe64b70c0a955bb25d9ddccd03c5cf9ed
Poly Titova <poly_titova@mail.ru>
2021-02-22 10:09:07 +0300

    Сделали редактирование статей

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -2,9 +2,18 @@
 
 const { Router } = require(`express`);
 const articlesRoutes = new Router();
+const api = require(`../api`).getAPI();
 
 articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRoutes.get(`/edit/:id`, async (req, res) => {
+  const { id } = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories()
+  ]);
+  res.render(`edit-post`, { article, categories });
+});
 articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRoutes;
