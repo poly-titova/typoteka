2475f5b236d490f44598556253f126b36f2864f1
Poly Titova <poly_titova@mail.ru>
2021-03-21 10:59:43 +0300

    Добавили поля для хранения идентификаторов

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE categories(
+  id integer NOT NULL,
   name varchar(255) NOT NULL
 );
 
 CREATE TABLE users(
+  id integer NOT NULL,
   email varchar(255) NOT NULL,
   password_hash varchar(255) NOT NULL,
   first_name varchar(255) NOT NULL,
@@ -11,14 +13,19 @@ CREATE TABLE users(
 );
 
 CREATE TABLE articles(
+  id integer NOT NULL,
   title varchar(255) NOT NULL,
   announce text NOT NULL,
   full_text text NOT NULL,
   picture varchar(50) NOT NULL,
+  user_id integer NOT NULL,
   created_at timestamp NOT NULL
 );
 
 CREATE TABLE comments(
+  id integer NOT NULL,
+  offer_id integer NOT NULL,
+  user_id integer NOT NULL,
   text text NOT NULL,
   created_at timestamp NOT NULL
 );
