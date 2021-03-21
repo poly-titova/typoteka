0ba054bbca7769025b428851aa7127fd55b3fa14
Poly Titova <poly_titova@mail.ru>
2021-03-21 11:59:10 +0300

    Добавили последовательности и индексы

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE categories(
-  id integer PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name varchar(255) NOT NULL
 );
 
 CREATE TABLE users(
-  id integer PRIMARY KEY,
-  email varchar(255) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  email varchar(255) UNIQUE NOT NULL,
   password_hash varchar(255) NOT NULL,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
@@ -13,22 +13,22 @@ CREATE TABLE users(
 );
 
 CREATE TABLE articles(
-  id integer PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   title varchar(255) NOT NULL,
   announce text NOT NULL,
   full_text text NOT NULL,
   picture varchar(50),
   user_id integer NOT NULL,
-  created_at timestamp NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments(
-  id integer PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   article_id integer NOT NULL,
   user_id integer NOT NULL,
   text text NOT NULL,
-  created_at timestamp NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (article_id) REFERENCES articles(id)
 );
@@ -40,3 +40,5 @@ CREATE TABLE article_categories(
   FOREIGN KEY (article_id) REFERENCES articles(id),
   FOREIGN KEY (category_id) REFERENCES categories(id)
 );
+
+CREATE INDEX ON articles (title);
