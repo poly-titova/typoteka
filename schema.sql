92af1d22401d51f5f5a6e77cde867cfee7362546
Poly Titova <poly_titova@mail.ru>
2021-03-21 11:55:38 +0300

    Указали ключи

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE categories(
-  id integer NOT NULL,
+  id integer PRIMARY KEY,
   name varchar(255) NOT NULL
 );
 
 CREATE TABLE users(
-  id integer NOT NULL,
+  id integer PRIMARY KEY,
   email varchar(255) NOT NULL,
   password_hash varchar(255) NOT NULL,
   first_name varchar(255) NOT NULL,
@@ -13,24 +13,30 @@ CREATE TABLE users(
 );
 
 CREATE TABLE articles(
-  id integer NOT NULL,
+  id integer PRIMARY KEY,
   title varchar(255) NOT NULL,
   announce text NOT NULL,
   full_text text NOT NULL,
-  picture varchar(50) NOT NULL,
+  picture varchar(50),
   user_id integer NOT NULL,
-  created_at timestamp NOT NULL
+  created_at timestamp NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments(
-  id integer NOT NULL,
-  offer_id integer NOT NULL,
+  id integer PRIMARY KEY,
+  article_id integer NOT NULL,
   user_id integer NOT NULL,
   text text NOT NULL,
-  created_at timestamp NOT NULL
+  created_at timestamp NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (article_id) REFERENCES articles(id)
 );
 
 CREATE TABLE article_categories(
   article_id integer NOT NULL,
-  category_id integer NOT NULL
+  category_id integer NOT NULL,
+  PRIMARY KEY (article_id, category_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
 );
