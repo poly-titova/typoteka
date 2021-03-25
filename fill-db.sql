416991847e4c5b073047080a9323488c1d63f8e3
Poly Titova <poly_titova@mail.ru>
2021-03-25 10:42:15 +0300

    Создали несколько категорий

--- fill-db.sql
+++ fill-db.sql
@@ -2,3 +2,9 @@
 INSERT INTO users(email, password_hash, first_name, last_name, avatar) VALUES
 ('ivanov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Иван', 'Иванов', 'avatar1.jpg'),
 ('petrov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Пётр', 'Петров', 'avatar2.jpg');
+
+-- эти запросы необходимы для заполнения в таблицу с категориями данными
+INSERT INTO categories(name) VALUES
+('За жизнь'),
+('Без рамки'),
+('Разное');
