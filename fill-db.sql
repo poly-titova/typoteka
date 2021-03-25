8b708f28df047a0fb5c71d4c7bfb459a7b6def96
Poly Titova <poly_titova@mail.ru>
2021-03-25 10:37:35 +0300

    Создали двоих пользователей

---
+++ fill-db.sql
@@ -0,0 +1,4 @@
+-- эти запросы необходимы для заполнения в таблицу пользователей данными
+INSERT INTO users(email, password_hash, first_name, last_name, avatar) VALUES
+('ivanov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Иван', 'Иванов', 'avatar1.jpg'),
+('petrov@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Пётр', 'Петров', 'avatar2.jpg');
