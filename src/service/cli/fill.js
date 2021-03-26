0d684214eb8813a65989ef6ac6898e33b660491d
Poly Titova <poly_titova@mail.ru>
2021-03-26 13:18:38 +0300

    Добавили двоих пользователей

--- src/service/cli/fill.js
+++ src/service/cli/fill.js
@@ -77,5 +77,23 @@ module.exports = {
 
     const [count] = args;
     const countArticle = Number.parseInt(count, 10) || DEFAULT_COUNT;
+
+    // Заранее заданные пользователи
+    const users = [
+      {
+        email: `ivanov@example.com`,
+        passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
+        firstName: `Иван`,
+        lastName: `Иванов`,
+        avatar: `avatar1.jpg`
+      },
+      {
+        email: `petrov@example.com`,
+        passwordHash: `5f4dcc3b5aa765d61d8327deb882cf99`,
+        firstName: `Пётр`,
+        lastName: `Петров`,
+        avatar: `avatar2.jpg`
+      }
+    ];
   }
 };
