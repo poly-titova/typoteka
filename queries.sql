a6a734762ec766af3f8027b5f910c587e57731e3
Poly Titova <poly_titova@mail.ru>
2021-04-03 10:33:51 +0300

    Получили список категорий для которых создана минимум одна публикация

--- queries.sql
+++ queries.sql
@@ -1,2 +1,8 @@
 -- запрос для получения списка всех категорий
 SELECT * FROM categories
+
+-- запрос для получения списка непустых категорий
+SELECT id, name FROM categories
+  JOIN article_categories
+  ON id = category_id
+  GROUP BY id
