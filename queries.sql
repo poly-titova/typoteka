06e2043a7ed28161ecd298522f34ce576ce0467c
Poly Titova <poly_titova@mail.ru>
2021-04-03 10:35:17 +0300

    Получили список категорий с количеством публикаций

--- queries.sql
+++ queries.sql
@@ -6,3 +6,9 @@ SELECT id, name FROM categories
   JOIN article_categories
   ON id = category_id
   GROUP BY id
+
+-- запрос для получения категорий с количеством публикаций
+SELECT id, name, count(article_id) FROM categories
+  LEFT JOIN article_categories
+  ON id = category_id
+  GROUP BY id
