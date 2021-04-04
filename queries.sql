5e82fdb664cff78ea3315c04bb3ef7a9de11a40d
Poly Titova <poly_titova@mail.ru>
2021-04-04 10:21:24 +0300

    Получили полную информацию определённой публикации

--- queries.sql
+++ queries.sql
@@ -27,3 +27,18 @@ FROM articles
   JOIN users ON users.id = articles.user_id
   GROUP BY articles.id, users.id
   ORDER BY articles.created_at DESC
+
+-- запрос для детальной информации по публикации
+SELECT articles.*, 
+  COUNT(comments.id) AS comments_count, 
+  STRING_AGG(DISTINCT categories.name, ', ') AS category_list,
+  users.first_name,
+  users.last_name,
+  users.email
+FROM articles
+  JOIN article_categories ON articles.id = article_categories.article_id
+  JOIN categories ON article_categories.category_id = categories.id
+  LEFT JOIN comments ON comments.article_id = articles.id
+  JOIN users ON users.id = articles.user_id
+WHERE articles.id = 1
+  GROUP BY articles.id, users.id
