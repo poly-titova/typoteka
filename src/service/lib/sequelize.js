82ae9e2a5503684ac703e14f8fa364fe231c932c
Poly Titova <poly_titova@mail.ru>
2021-04-16 20:45:45 +0300

    Создали и экспортировали экземпляр Sequelize, передав в конструктор всё, что нужно

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -8,3 +8,19 @@ const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].
 if (somethingIsNotDefined) {
   throw new Error(`One or more environmental variables are not defined`);
 }
+
+module.exports = new Sequelize(
+  DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    // указываем, с какой СУБД предстоит работать
+    dialect: `postgres`,
+    // настройки пула соединений
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 10000,
+      idle: 10000
+    }
+  }
+);
