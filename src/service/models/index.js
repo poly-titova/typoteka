9b3c3b0b210d525c9cb840f2a3eb09994b22243c
Poly Titova <poly_titova@mail.ru>
2021-06-28 10:28:30 +0300

    Создали модель пользователя

--- src/service/models/index.js
+++ src/service/models/index.js
@@ -4,6 +4,7 @@ const { Model } = require(`sequelize`);
 const defineCategory = require(`./category`);
 const defineComment = require(`./comment`);
 const defineArticle = require(`./article`);
+const defineUser = require(`./user`);
 
 // импортируем перечисление
 const Aliase = require(`./aliase`);
@@ -14,9 +15,16 @@ const define = (sequelize) => {
   const Category = defineCategory(sequelize);
   const Comment = defineComment(sequelize);
   const Article = defineArticle(sequelize);
+  const User = defineUser(sequelize);
 
   Article.hasMany(Comment, { as: Aliase.COMMENTS, foreignKey: `articleId` });
   Comment.belongsTo(Article, { foreignKey: `articleId` });
+
+  User.hasMany(Article, { as: Aliase.ARTICLES, foreignKey: `userId` });
+  Article.belongsTo(User, { as: Aliase.USER, foreignKey: `userId` });
+
+  User.hasMany(Comment, { as: Aliase.COMMENTS, foreignKey: `userId` });
+  Comment.belongsTo(User, { as: Aliase.USER, foreignKey: `userId` })
 
   ArticleCategory.init({}, { sequelize });
 
@@ -24,7 +32,7 @@ const define = (sequelize) => {
   Category.belongsToMany(Article, { through: `articleCategories`, as: Aliase.ARTICLES });
   Category.hasMany(ArticleCategory, { as: Aliase.ARTICLE_CATEGORIES });
 
-  return { Category, Comment, Article, ArticleCategory };
+  return { Category, Comment, Article, ArticleCategory, User };
 }
 
 module.exports = define;
