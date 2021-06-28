f1880770ec49506a81b852194a02aa75d2a11d40
Poly Titova <poly_titova@mail.ru>
2021-06-28 10:58:57 +0300

    Создали новый сервис пользователь

--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -4,10 +4,12 @@ const ArticleService = require(`./articles`);
 const CategoryService = require(`./category`);
 const CommentService = require(`./comment`);
 const SearchService = require(`./search`);
+const UserService = require(`./user`);
 
 module.exports = {
   ArticleService,
   CategoryService,
   CommentService,
-  SearchService
+  SearchService,
+  UserService
 };
