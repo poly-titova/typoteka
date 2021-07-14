d6f142b68a2c7e4630bbaaebe22be6a9f6bfac90
Poly Titova <poly_titova@mail.ru>
2021-07-14 11:58:51 +0300

    Добавили хранение в сессии данные текущего пользователя

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
 
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -11,6 +12,31 @@ const DEFAULT_PORT = 8080;
 const app = express();
 
 const path = require(`path`);
+
+const sequelize = require(`../service/lib/sequelize`);
+
+const { SESSION_SECRET } = process.env;
+if (!SESSION_SECRET) {
+    throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
+const mySessionStore = new SequelizeStore({
+    db: sequelize,
+    expiration: 180000,
+    checkExpirationInterval: 60000
+});
+
+sequelize.sync({ force: false });
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: SESSION_SECRET,
+    store: mySessionStore,
+    resave: false,
+    proxy: true,
+    saveUninitialized: false,
+  }));
 
 const PUBLIC_DIR = `public`;
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
